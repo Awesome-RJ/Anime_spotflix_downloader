b507275fcd84bb4dda4918e8337a22212bcb9eca
animespotflix <79033810+animespotflix@users.noreply.github.com>
2021-02-14 09:53:03 +0530

    Create downloaderanime.py

---
+++ downloaderanime.py
@@ -0,0 +1,16 @@
+from animespot import mp4upload
+from animespot import vidcdn
+
+
+def get_available_download_link(episode_dict):
+	if 'mp4' in episode_dict['embed-servers'].keys():
+		return mp4upload.get_mp4upload_download_link(episode_dict['embed-servers']['mp4'])
+	if 'vidcdn' in episode_dict['embed-servers'].keys():
+		return vidcdn.get_vidcdn_download_link(episode_dict['embed-servers']['vidcdn'])
+	return 'unavailable'
+
+def main():
+	print('This is an import script')
+
+if __name__ == '__main__':
+	main()
